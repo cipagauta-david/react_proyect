b8d69240ea49af6d0746aea0a49bad22d3109d44
David Jiménez <skyrroner@gmail.com>
2022-08-03 13:46:32 -0500

    Pruebas

---
+++ src/Components/Inicio.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from '../logo.svg';
+
+export const Inicio = () => {
+  return (
+    
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        PRUEBAAAA
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  )
+}
